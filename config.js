5bcf6d28584de8a2ac29f9baef91a45a01f456b8
Sander Crevits <sander.crevits@hotmail.com>
2018-08-11 11:38:37 +0200

    mongodb url

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     'secret':   'test',
     HOST: 'http://localhost',
     PORT: 3000,
-    MONGODBURL : process.env.MONGO_URI || 'mongodb://localhost:27017/herexamensEcoplan',
+    MONGODBURL : process.env.MONGO_URI || 'mongodb://heroku_2m67fgcb:ketc64dibbr9jm3hd7gq1ebbur@ds119422.mlab.com:19422/heroku_2m67fgcb', //mongodb://localhost:27017/herexamensEcoplan
     ECOPLAN: 'https://datatank.stad.gent/4/milieuennatuur/ecoplan.json'
 }
